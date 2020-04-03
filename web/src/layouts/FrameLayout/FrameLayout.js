caf32f1177a356d6752e48852e46af39dac9f68b
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-03 19:00:26 -0400

    Set up variables style sheet

--- web/src/layouts/FrameLayout/FrameLayout.js
+++ web/src/layouts/FrameLayout/FrameLayout.js
@@ -2,19 +2,22 @@ import { Link, routes } from '@redwoodjs/router'
 
 const FrameLayout = ({ children }) => {
   return (
-    <header>
-      <nav>
-        <Link to={routes.home()}>
-          <h2>Logo</h2>
-        </Link>
-        <div className="navlinks">
-          <Link to={routes.home()}>home</Link>
-          <Link to={routes.sales()}>Sales</Link>
-        </div>
-      </nav>
-
-      {children}
-    </header>
+    <>
+      <header>
+        <nav>
+          <div className="logo">
+            <Link to={routes.home()}>
+              <h2>Logo</h2>
+            </Link>
+          </div>
+          <div className="navlinks">
+            <Link to={routes.home()}>home</Link>
+            <Link to={routes.sales()}>Sales</Link>
+          </div>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
   )
 }
 
