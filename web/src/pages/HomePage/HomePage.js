8c92ac667839b74040d8acdd0fa36191c3596b63
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-03 20:00:08 -0400

    Updated sttles on main page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,9 +1,30 @@
 import FrameLayout from 'src/layouts/FrameLayout'
+
+import img from 'src/images/profile_pic.jpg'
 const HomePage = () => {
   return (
     <FrameLayout>
       <h1>HomePage</h1>
-      <p>Bunnies</p>
+      <h2>Bunnies</h2>
+      <img src={img} alt="Profile" height="300px" />
+      <div className="cardList">
+        <div className="bunnyCard">
+          <h3>Somebunny</h3>
+          <p>This is a bunny card</p>
+        </div>
+        <div className="bunnyCard">
+          <h3>Somebunny</h3>
+          <p>This is a bunny card</p>
+        </div>
+        <div className="bunnyCard">
+          <h3>Somebunny</h3>
+          <p>This is a bunny card</p>
+        </div>
+        <div className="bunnyCard">
+          <h3>Somebunny</h3>
+          <p>This is a bunny card</p>
+        </div>
+      </div>
     </FrameLayout>
   )
 }
