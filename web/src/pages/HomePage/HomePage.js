115767d6e2c0f4f851fbb4806cf4b939da26e3ee
Jason Sonnichsen <Harliquinn@gmail.com>
2020-04-10 14:02:45 -0400

    Homepage Layout First Pass

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -4,9 +4,30 @@ import img from 'src/images/profile_pic.jpg'
 const HomePage = () => {
   return (
     <FrameLayout>
-      <h1>HomePage</h1>
-      <h2>Bunnies</h2>
-      <img src={img} alt="Profile" height="300px" />
+      <h1>Welcome to Underhill Rabbitry</h1>
+      <img className="profile" src={img} alt="Profile" height="300px" />
+      <p>
+        Straw augers beef kettle our crickets. Grapes at yams mushrooms organic
+        berries gobble. In the straw rain barrels. Augers oats hen cowpies.
+        Garden windmill chicks, hen at corn in, lettus a peppers. Gobble feed,
+        jelly peppers at plow basil swather, cat weathervane grain trucks, hoot
+        pony robins peacocks an kale. Goos.
+      </p>
+      <p>
+        Hammers cattle rats in crows. Haybine carrots soybeans, owls duck
+        raising or, cheep in plows. Baa potato donkey mouse, at gate grain bins
+        woof. Ewes mushrooms zucchini in forage Harvester at sheep with tractor.
+        Onion organic oranges and purr ducks canning owls at a squeal. Oranges
+        cucumbers rhubarb gourds watermelon. Baa potato donke.
+      </p>
+      <p>
+        Mouse, at gate grain bins woof. Goose hammers cattle rats in crows.
+        Bulls at rose garden cucumbers mice sunflower wheat in pig. John Deere
+        bees, parsley sweet corn at, porky pig shovels. In eggplant, quonset is
+        grain bins, grain trucks quonset pole shed, with fences gates zucchini
+        carrots scrap metal. Ewes mushrooms zucchini in forage Harvester at
+        sheep with tractor. Augers oats hen cowpies. Forage Harvester, bean an.
+      </p>
     </FrameLayout>
   )
 }
