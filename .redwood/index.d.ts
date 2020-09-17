1fce71be3bcd2fbe15d5ad1f7f1056f2595d6ad6
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-17 13:25:09 -0400

    v17

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,2 +1,4 @@
-// 2020-09-16T19:04:38.460Z
+// 2020-09-17T17:24:15.094Z
+/// <reference path="./types/import-dir-schemas.d.ts" />
+/// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
