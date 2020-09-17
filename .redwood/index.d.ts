4f186269e4dd274a0f8c6c8f50daa61e49edfa5f
Jason Sonnichsen <Harliquinn@gmail.com>
2020-09-17 13:34:12 -0400

    Redwood updated to newest version v18

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2020-09-17T17:24:15.094Z
+// 2020-09-17T17:32:32.064Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
